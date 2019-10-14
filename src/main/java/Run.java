659c0a99b073765846bf0b191a322170dcb08dfd
Chadha <shatha.degachi@gmail.com>
2019-10-14 16:51:52 +0100

    class length counter updated

--- src/main/java/Run.java
+++ src/main/java/Run.java
@@ -1,35 +1,39 @@
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.visitor.VoidVisitor;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 public class Run {
 
     //TODO automate scan and adding files in path
-    private static final String[] FILE_PATHS = new String[] {"src/main/java/testcode/Bloaters/Grid.java", "src/main/java/testcode/FalsePositives/BoxingTheCompass.java",
+    private static final String[] FILE_PATHS = new String[]{"src/main/java/testcode/Bloaters/Grid.java", "src/main/java/testcode/FalsePositives/BoxingTheCompass.java",
             "src/main/java/testcode/Abusers/RefusedBequest/Account.java", "src/main/java/testcode/Abusers/RefusedBequest/ChqAcc.java",
             "src/main/java/testcode/Abusers/RefusedBequest/SavingsAcc.java", "src/main/java/testcode/Abusers/BarnsleyFernTwo.java"};
 
     private static final Logger log = Logger.getLogger(Run.class.getName());
+    private static List<Integer> methodLengths = new ArrayList<>();
 
     public static void main(String[] args) {
         analyseFiles();
     }
 
     private static void analyseFiles() {
-        Integer count = 0;
         try {
-            for(String FILE_PATH : FILE_PATHS){
-                log.info("Analysing class: " +  FILE_PATH.substring(FILE_PATH.lastIndexOf('/') + 1));
+            for (String FILE_PATH : FILE_PATHS) {
+                methodLengths.clear(); // clear list of method lengths for every new file processed
+                log.info("Analysing class: " + FILE_PATH.substring(FILE_PATH.lastIndexOf('/') + 1));
                 CompilationUnit cu = StaticJavaParser.parse(new FileInputStream(FILE_PATH));
                 VoidVisitor<?> methodParameterCounter = new MethodParameterCounter();
-                VoidVisitor<Integer> methodLengthCounter = new MethodLengthCounter();
+                VoidVisitor<List<Integer>> methodLengthCounter = new MethodLengthCounter();
                 VoidVisitor<?> classLengthCounter = new ClassLengthCounter();
 
                 methodParameterCounter.visit(cu, null);
-                methodLengthCounter.visit(cu, count);
+                methodLengthCounter.visit(cu, methodLengths);
                 classLengthCounter.visit(cu, null);
             }
 
